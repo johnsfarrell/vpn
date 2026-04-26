e57e029de62e76f9830ebce468d44c867b8cd799
John Farrell <jsfarrell12@gmail.com>
2026-04-26 14:25:09 -0400

    fix: client setup

--- wireguard/scripts/client_setup.sh
+++ wireguard/scripts/client_setup.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 DEVICE_NAME="${1:?Usage: $0 <device_name>}"
-CLIENT_DIR="$HOME/wireguard/clients/$DEVICE_NAME"
-PUBLIC_IP_ADDRESS=$(curl ifconfig.me)
-CLIENT_IP="$("$HOME/wireguard/scripts/find_free_client_ip.sh")"
+CLIENT_DIR="./wireguard/clients/$DEVICE_NAME"
+PUBLIC_IP_ADDRESS=$(curl -fsS ifconfig.me)
+CLIENT_IP="$("./wireguard/scripts/find_free_client_ip.sh")"
 
 
 mkdir -p "$CLIENT_DIR"
@@ -19,19 +19,18 @@ AllowedIPs = '"$CLIENT_IP"'
 EOF'
 
 sudo systemctl restart wg-quick@wg0
-sudo wg
 
-sudo bash -c 'cat > "$CLIENT_DIR/client.conf" <<EOF
+cat > "$CLIENT_DIR/client.conf" <<EOF
 [Interface]
-PrivateKey = "$(cat "$CLIENT_DIR/client_private.key")"
-Address = "$CLIENT_IP"
+PrivateKey = $(cat "$CLIENT_DIR/client_private.key")
+Address = $CLIENT_IP
 DNS = 1.1.1.1
 
 [Peer]
-PublicKey = "$(cat /etc/wireguard/server_public.key)"
-Endpoint = "$PUBLIC_IP_ADDRESS:51820"
-AllowedIPs = 0.0.0.0/0, ::/0
+PublicKey = $(sudo cat /etc/wireguard/server_public.key)
+Endpoint = $PUBLIC_IP_ADDRESS:51820
+AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 25
-EOF'
+EOF
 
 qrencode -t ansiutf8 < "$CLIENT_DIR/client.conf"
