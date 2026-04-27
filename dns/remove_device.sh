1458b294cdc6f1356a6d98a95ee729bd057eae09
John Farrell <jsfarrell12@gmail.com>
2026-04-27 01:28:18 -0400

    fix: double clicking + remove device dns

--- dns/remove_device.sh
+++ dns/remove_device.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-DEVICE_NAME="${1:?Usage: $0 <device_name>}"
+NAME="${1:?Usage: $0 <device_name>}"
 VPN_DOMAIN="${VPN_DOMAIN:-vpn.io}"
 
-sudo sed -i "/^.*[[:space:]]$DEVICE_NAME\.$VPN_DOMAIN$/d" /etc/dnsmasq.hosts
+sudo sed -i "/^.*[[:space:]]$NAME\.$VPN_DOMAIN[[:space:]]$NAME$/d" /etc/dnsmasq.hosts
 
 sudo systemctl restart dnsmasq
